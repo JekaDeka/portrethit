7832fe2df4910e57cdea3ff86b0a76137f1c6717
JekaDeka <SpecialForWarcraft@gmail.com>
2016-04-13 22:08:46 +0300

    portrethit

--- portrethit/views.py
+++ portrethit/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
+from django.utils import timezone
+from django.http import HttpResponse
 
-# Create your views here.
+
+def main_page(request):
+    return render(request, 'portrethit/index.html', {})
