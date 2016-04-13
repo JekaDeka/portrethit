ec67afc3796bf7f1c288f2375dfab97485f94885
JekaDeka <SpecialForWarcraft@gmail.com>
2016-04-13 19:19:39 +0300

    portrethit

---
+++ portrethit/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PortrethitConfig(AppConfig):
+    name = 'portrethit'
--- portrets/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PortretsConfig(AppConfig):
-    name = 'portrets'
