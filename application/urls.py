4f45df6e3672ac19bf72be20a7a60f409a594272
JekaDeka <SpecialForWarcraft@gmail.com>
2016-05-28 22:08:54 +0300

    portrethit

--- application/urls.py
+++ application/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     #url(r'', include('portrethit.urls')),
     url(r'', include('wowportret.urls')),
-    url(r'^photologue/', include('photologue.urls', namespace='photologue')),
+    #url(r'^photologue/', include('photologue.urls', namespace='photologue')),
 ]
