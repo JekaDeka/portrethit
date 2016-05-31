dd08cbd6ac44a42f990331117510f4fa1f2b1709
JekaDeka <SpecialForWarcraft@gmail.com>
2016-06-01 00:11:44 +0300

    portrethit

--- wowportret/models.py
+++ wowportret/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 from django.forms import ModelForm
-from django.utils.encoding import python_2_unicode_compatible
+from django.utils.crypto import get_random_string
+import re
 
 
 def content_file_name(instance, filename):
-    return 'tmp_img/'.join(['mail_%Y%m%d', filename])
+    ext = filename.split('.')
+    filename = get_random_string(length=32) + "." + ext[1]
+    return '/'.join(['tmp_img', filename])
 
 
-@python_2_unicode_compatible
 class Document(models.Model):
     docfile = models.FileField(upload_to=content_file_name)
-
-    def __str__(self):
-        return self.name
