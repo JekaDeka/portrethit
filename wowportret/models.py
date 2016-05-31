939c375c8513367b4b3d92244aa673d2c07df943
JekaDeka <SpecialForWarcraft@gmail.com>
2016-05-31 23:56:24 +0300

    portrethit

--- wowportret/models.py
+++ wowportret/models.py
@@ -6,3 +6,6 @@ from django.utils.encoding import python_2_unicode_compatible
 @python_2_unicode_compatible
 class Document(models.Model):
     docfile = models.FileField(upload_to='tmp_img/')
+
+    def __str__(self):
+        return self.name
