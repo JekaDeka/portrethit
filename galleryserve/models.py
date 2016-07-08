628ad3295ca6cab9e96d1b505b6dc8bba7478180
JekaDeka <SpecialForWarcraft@gmail.com>
2016-07-09 01:17:31 +0300

    portrethit

--- galleryserve/models.py
+++ galleryserve/models.py
@@ -119,11 +119,8 @@ class Item(models.Model):
     class Meta:
         ordering = ('sort',)
 
-    def __str__(self):
+    def __unicode__(self):
         return u'%s' % (self.alt)
-
-    # def __unicode__(self):
-    #     return u'%s' % (self.alt)
 
     def save(self, **kwargs):
         super(Item, self).save()
