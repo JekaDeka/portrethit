0dc0a25e6058d5f1e6e367c7ffdf61d3af42d5dd
JekaDeka <SpecialForWarcraft@gmail.com>
2016-07-27 19:31:20 +0300

    portrethit

---
+++ wowportret/static/js/module-image-creator.js
@@ -0,0 +1,162 @@
+$(document).ready(function() {
+    var c = document.getElementById("myCanvas");
+    var mini_img = document.getElementById("main-img");
+    var ctx = c.getContext("2d");
+    var width = c.width;
+    var height = c.height;
+    var offset = width / 15;
+    ctx.lineWidth = offset / 2;
+    ctx.strokeStyle = "#444";
+    ctx.fillStyle = "#444";
+    //ctx.globalAlpha = 0.8;
+
+    window.onload = function() {
+        createImage();
+    }
+
+    function createImage() {
+        //Get src of full sized image
+        // var src = $(mini_img).attr('src');
+        // var tmp = src.split('.');
+        // src = tmp[0] + '.' + tmp[2];
+        var src = mini_img.src
+
+        //create full sized image
+        var img = document.createElement('img');
+        img.src = src;
+        img.alt = mini_img.alt;
+        img.title = mini_img.title
+            // $(img).addClass('img-responsive');
+            // c.appendChild(img);
+
+        $(c).width(img.width);
+
+        ctx.imageSmoothingEnabled = false;
+        ctx.drawImage(img, 4, 4);
+
+        //Turn transparency on
+
+        // drawRectangle();
+
+    }
+
+
+
+    function drawHorizonRectangle(sprt_by) {
+
+        var start_point = 0;
+        var cnt = 0;
+        while (cnt < sprt_by) {
+            ctx.strokeRect(start_point, 0, width / sprt_by, height);
+            start_point += width / sprt_by;
+            cnt++;
+        }
+    }
+
+    function drawVerticalRectangle(sprt_by) {
+        var start_point = 0;
+        var cnt = 0;
+        while (cnt < sprt_by) {
+            ctx.strokeRect(0, start_point, width, height / sprt_by);
+            start_point += height / sprt_by;
+            cnt++;
+        }
+    }
+
+    function drawGrid(from_x, from_y, cell_x, cell_y, to_x, to_y) {
+        var cell_width = (to_x - from_x) / cell_x;
+        var cell_height = (to_y - from_y) / cell_y;
+
+        for (var i = from_x; i < to_x; i += cell_width) {
+            for (var j = from_y; j < to_y; j += cell_height) {
+                ctx.strokeRect(i, j, cell_width, cell_height);
+            }
+        }
+    }
+
+    function drawCustomGrid(type) {
+        if (type == 0) {
+            ctx.strokeRect(0, 0, width / 2, height);
+            drawGrid(width / 2, 0, 4, 2);
+        }
+
+        if (type == 1) {
+            ctx.strokeRect(0, 0, width / 5, height); //left part
+            drawGrid(width / 5, 0, 1, 3, width - width / 5, height);
+            ctx.strokeRect(width - width / 5, 0, width / 5, height); //right part
+        }
+        if (type == 2) {
+            drawGrid(0, 0, 1, 3, width - width / 5, height);
+            ctx.strokeRect(width - width / 5, 0, width / 5, height); //right part
+        }
+        if (type == 3) {
+            ctx.strokeRect(0, 0, width / 5, height); //left part
+            ctx.strokeRect(width / 5, 0, width - width / 5, height / 3); //top part
+            ctx.strokeRect(width - width / 5, height / 3, width / 5, height - height / 3); //right part
+            ctx.strokeRect(width / 5, height / 3, width - width / 5, height - height / 3); //bottom part
+        }
+        if (type == 4) {
+            ctx.strokeRect(0, 0, width / 5, height); //left part
+            ctx.strokeRect(width - width / 5, 0, width / 5, height); //right part
+            drawGrid(width / 5, 0, 1, 1, width - width / 5, height / 3); //top
+            ctx.strokeRect(width / 5, height / 3, width - 2 * width / 5, height - height / 3); //bottom
+        }
+        if (type == 5) {
+            ctx.strokeRect(0, 0, width / 5, height); //left part
+            ctx.strokeRect(width - width / 5, 0, width / 5, height); //right part
+            drawGrid(width / 5, 0, 1, 1, width - width / 5, height / 3); //top
+            drawGrid(width / 5, height / 3, 2, 1, width - width / 5, height); //bottom
+
+        }
+        if (type == 6) {
+            ctx.strokeRect(0, 0, width / 2, height - height / 3);
+            ctx.strokeRect(0, height - height / 3, width, height / 3);
+            drawGrid(width / 2, 0, 3, 1, width, height - height / 3);
+        }
+
+        if (type == 7) {
+            drawGrid(0, 0, 4, 1, width, height - height / 2.7);
+            ctx.strokeRect(0, height - height / 2.7, width, height / 2.7);
+        }
+        if (type == 8) {
+            ctx.strokeRect(0, 0, width, height / 4);
+            drawGrid(0, height / 4, 3, 1, width, height - height / 4);
+            ctx.strokeRect(0, height - height / 4, width, height / 4);
+        }
+
+        if (type == 9) {
+            ctx.strokeRect(0, 0, width - width / 3, height);
+            drawGrid(width - width / 3, 0, 1, 3, width, height);
+        }
+
+        if (type == 10) {
+            ctx.strokeRect(width - width / 3, 0, width / 3, height / 2);
+            ctx.fillRect(0, 0, width / 3, height / 2);
+            ctx.strokeRect(width / 3, 0, width / 3, height);
+            ctx.strokeRect(0, height - height / 2, width / 3, height / 2);
+            ctx.fillRect(width - width / 3, height / 2, width / 3, height / 2);
+        }
+        if (type == 11) {
+            drawGrid(0, 0, 1, 2, width / 3, height);
+            ctx.strokeRect(width / 3, 0, width / 3, height);
+            drawGrid(width - width / 3, 0, 1, 2, width, height);
+        }
+        if (type == 12) {
+            ctx.strokeRect(width / 3, 0, width / 3, height);
+        }
+    }
+
+
+
+    $('#test_button').click(function() {
+        var k = 0;
+        while (k < 7) {
+            drawCustomGrid(5);
+            k++;
+        }
+    });
+    $('#clear_button').click(function() {
+        ctx.clearRect(0, 0, c.width, c.height);
+        createImage();
+    });
+});
