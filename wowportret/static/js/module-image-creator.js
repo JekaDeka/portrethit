aa77b66cf7895a774489b780adbfda8cc8825b43
JekaDeka <SpecialForWarcraft@gmail.com>
2016-07-28 00:27:52 +0300

    portrethit

--- wowportret/static/js/module-image-creator.js
+++ wowportret/static/js/module-image-creator.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 
 
         ctx.strokeStyle = gradient;
-        ctx.fillStyle = 'gray';
+        ctx.fillStyle = 'white';
 
         ctx.shadowBlur = 3;
         ctx.shadowOffsetX = 2;
