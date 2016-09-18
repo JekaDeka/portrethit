4a4de688cc00d1b19a77bf2902b45b413247cbe4
007raspc <007raspc@gmail.com>
2016-09-18 04:49:04 +0300

    summ 18.09.2016 04

--- wowportret/static/js/custum.js
+++ wowportret/static/js/custum.js
@@ -5,7 +5,16 @@ jQuery(function(f){
     });
 });
 
-$( "#callback" ).click(function() {
+$( "#callback_a" ).click(function() {
   $( '#id_content' ).css('display','none');
 });
 
+$( "#callback" ).click(function() {
+    var textarea_input = $('#id_content').parent();
+    $(textarea_input).css('display','none')
+});
+
+$( "#callback" ).click(function() {
+    var email_input = $('#id_contact_email').parent();
+    $(email_input).css('display','none')
+});
